722f9131f6ccb46f8a24dabfd0a9fa148f60b422
asialeaf <1355671089@qq.com>
2022-07-19 19:48:25 +0800

    Update gitops

--- cmd/gitops.sh
+++ cmd/gitops.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
+# GITSOURCE="https://github.com/asialeaf/gitops.git"
+# GITPATH="deploy/"
+# CALLBACK="http://10.96.221.127:8060/dingtalk/webhook1/send"
 
+TEMP_DIR=/tmp/git_storage
 echo "Hello..."
-kubectl version
-git version
-echo "end..."
+# Create temp dir
+mkdir -p $TEMP_DIR
+# Get the Git repository
+SUFF=${GITSOURCE##*/}
+GITREPONAME=${SUFF%%.*}
+git clone $GITSOURCE $TEMP_DIR/$GITREPONAME
+
+echo "Git Repo "$GITREPONAME" "
+
+# kubectl apply
+cd $TEMP_DIR/$GITREPONAME/$GITPATH
+kubectl apply -f .
+
+# Send msg
+DATA='{"status": "success"}'
+
+if [[ $? == 0 ]]; then
+    http_code=$(curl -m 10 -o /dev/null -s -w %{http_code} -H 'Content-Type: application/json' -d "$DATA" -X POST $CALLBACK)
+    if [[ $http_code == 200 ]]; then 
+        echo "msg send success!"
+    else 
+        echo "msg send failed!, http_code: "$http_code
+    fi
+fi
+echo "End..."
